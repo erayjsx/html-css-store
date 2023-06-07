f1a6a1eb655d8dece9106e0ba482c934ab436874
Eray Can Topcu <theressam52@gmail.com>
2023-06-07 20:59:54 +0300

    .

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,5 +1,6 @@
 function openModal() {
   document.getElementById("searchModal").style.display = "flex";
+  document.getElementById("searchModal").style.opacity = "1";
   document.querySelector("body").style.overflow = "hidden";
 
   var element = document.querySelector("nav");
@@ -8,6 +9,7 @@ function openModal() {
 
 function closeModal() {
   document.getElementById("searchModal").style.display = "none";
+  document.getElementById("searchModal").style.opacity = "0";
   document.querySelector("body").style.overflow = "auto";
 
   var element = document.querySelector("nav");
--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,19 +1,23 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "90%";
+    document.getElementById("myNav").style.opacity = "1";
+    document.getElementById("myNav").style.height = "85%";
     document.querySelector("body").style.overflow = "hidden";
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("myNav").style.opacity = "0";
+    document.getElementById("myNav").style.height = "0%";
     document.querySelector("body").style.overflow = "auto";
 }
 
 function openBasket() {
-    document.getElementById("basket").style.width = "90%";
+    document.getElementById("basket").style.opacity = "1";
+    document.getElementById("basket").style.height = "100%";
     document.querySelector("body").style.overflow = "hidden";
 }
 
 function closeBasket() {
-    document.getElementById("basket").style.width = "0%";
+    document.getElementById("basket").style.opacity = "0";
+    document.getElementById("basket").style.height = "0%";
     document.querySelector("body").style.overflow = "auto";
 }
